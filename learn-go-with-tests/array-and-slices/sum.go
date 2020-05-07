e77329ce3948251c34a392f46c12ff05266c8f3c
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 16:10:15 +0400

    Refactor SumAll function

--- learn-go-with-tests/array-and-slices/sum.go
+++ learn-go-with-tests/array-and-slices/sum.go
@@ -3,11 +3,9 @@ package main
 // Sum adds elements of an array and returns the result
 func Sum(numbers []int) int {
 	sum := 0
-
 	for _, number := range numbers {
 		sum += number
 	}
-
 	return sum
 }
 
@@ -15,13 +13,10 @@ func Sum(numbers []int) int {
 SumAll takes a varying number of slices and returns
 a new slice containing the totals for each slice passed in.
 */
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
-
 	return sums
 }
