eca0c19bef1fcbf506a3c6a7c292826db7d5b5c7
wildlyclassyprince <lihtumb@gmail.com>
2020-05-08 01:43:43 +0400

    Handle 'nil' accessors and errors with asssertError

--- learn-go-with-tests/pointers-and-errors/wallet_test.go
+++ learn-go-with-tests/pointers-and-errors/wallet_test.go
@@ -13,10 +13,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t *testing.T, err error) {
+	assertError := func(t *testing.T, got error, want string) {
 		t.Helper()
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
@@ -37,6 +41,6 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 	})
 }
