e6c104982e67ff506450e076a1e54b6a89646c12
wildlyclassyprince <lihtumb@gmail.com>
2020-05-08 01:00:32 +0400

    Test and create Wallet struct & methods

---
+++ learn-go-with-tests/pointers-and-errors/wallet.go
@@ -0,0 +1,16 @@
+package main
+
+// Wallet captures account transactions
+type Wallet struct {
+	balance int
+}
+
+// Deposit method
+func (w Wallet) Deposit(amount float64) int {
+	return 0
+}
+
+// Balance method
+func (w Wallet) Balance() int {
+	return 0
+}
