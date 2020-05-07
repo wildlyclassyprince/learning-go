35aaeb9ebd4d1b4dfdee7b343eccba750b32f3f9
wildlyclassyprince <lihtumb@gmail.com>
2020-05-08 01:17:26 +0400

    Add string formatting to printed output

--- learn-go-with-tests/pointers-and-errors/wallet.go
+++ learn-go-with-tests/pointers-and-errors/wallet.go
@@ -1,17 +1,31 @@
 package main
 
+import "fmt"
+
+// Stringer on Bitcoin
+type Stringer interface {
+	String() string
+}
+
+// Stringer method for printing string format
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// Bitcoin currency
+type Bitcoin int
+
 // Wallet captures account transactions
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit method
-func (w *Wallet) Deposit(amount int) int {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
-	return w.balance
 }
 
 // Balance method
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
