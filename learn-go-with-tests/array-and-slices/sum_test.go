07d654446e6987b06888eb4d170321766f782ced
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 15:47:53 +0400

    Test for any size collection

--- learn-go-with-tests/array-and-slices/sum_test.go
+++ learn-go-with-tests/array-and-slices/sum_test.go
@@ -3,12 +3,23 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %d want %d give, %v", got, want, numbers)
-	}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{3, 7, 9, 1, 6, 4, 2, 8, 5, 5}
+		got := Sum(numbers)
+		want := 50
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
