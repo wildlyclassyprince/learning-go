e749f327c68779b3884cad609c1e85448437d014
wildlyclassyprince <lihtumb@gmail.com>
2020-05-08 02:30:23 +0400

    Test for unknown words in the dictionary

--- learn-go-with-tests/maps/dictionary.go
+++ learn-go-with-tests/maps/dictionary.go
@@ -1,9 +1,16 @@
 package main
 
+import "errors"
+
 // Dictionary mapper
 type Dictionary map[string]string
 
 // Search retrieves the value of the specified key word
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
