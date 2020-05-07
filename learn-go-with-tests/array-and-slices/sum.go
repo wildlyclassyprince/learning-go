f8bbfc2a508b5125d50572e7d40b91f71544f443
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 15:50:43 +0400

    Test for any size slice rather than an array

--- learn-go-with-tests/array-and-slices/sum.go
+++ learn-go-with-tests/array-and-slices/sum.go
@@ -1,7 +1,7 @@
 package main
 
 // Sum adds elements of an array and returns the result
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
