dc28c212326d1a5c53d83b0546ff6e81855df171
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 19:30:45 +0400

    Test and create Area function

--- learn-go-with-tests/structs-methods-interfaces/shapes.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes.go
@@ -2,5 +2,10 @@ package main
 
 // Perimeter calculates the perimeter of a rectangle given a height and width.
 func Perimeter(width float64, height float64) float64 {
-	return 0
+	return 2 * (width + height)
+}
+
+// Area calculates the area of a rectangle given the height and width.
+func Area(width float64, height float64) float64 {
+	return width * height
 }
