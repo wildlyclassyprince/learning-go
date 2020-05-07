e03bebc89716433d28a0acd158fc6217845196d2
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 16:21:39 +0400

    Define SumAllTails function

--- learn-go-with-tests/array-and-slices/sum.go
+++ learn-go-with-tests/array-and-slices/sum.go
@@ -20,3 +20,20 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+/*
+SumAllTails calculates the totals of the "tails" of each slice.
+The tail of a collection is all the items apart from the first one (the "head").
+*/
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
