7ba04dbcf3162cc5731ec1c95a941643c3af48ff
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 20:05:33 +0400

    Test and create Area methods: rectangle & circle

--- learn-go-with-tests/structs-methods-interfaces/shapes.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes.go
@@ -1,11 +1,29 @@
 package main
 
+import "math"
+
 // Perimeter calculates the perimeter of a rectangle given a height and width.
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-// Area calculates the area of a rectangle given the height and width.
-func Area(width float64, height float64) float64 {
-	return width * height
+// Rectangle encapsulates the properties of a rectangle.
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Area method calculates the area of a rectangle given the width and height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle encapsulates the properties of a circle
+type Circle struct {
+	Radius float64
+}
+
+// Area method calculates the area of a circle given the radius
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
