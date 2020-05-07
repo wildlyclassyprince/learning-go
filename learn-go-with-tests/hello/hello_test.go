6f1b83a1ec437a733cf20756fce7ba2a4d5fb797
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 13:19:55 +0400

    Add French language greeting

--- learn-go-with-tests/hello/hello_test.go
+++ learn-go-with-tests/hello/hello_test.go
@@ -28,4 +28,10 @@ func TestHello(t *testing.T) {
 		want := "Hola Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Marco", "French")
+		want := "Bonjour Marco"
+		assertCorrectMessage(t, got, want)
+	})
 }
