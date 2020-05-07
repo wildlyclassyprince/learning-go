56645796347a75f4584fd0211bf09b8981eac047
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 12:58:24 +0400

    Refactor test message

--- learn-go-with-tests/hello/hello_test.go
+++ learn-go-with-tests/hello/hello_test.go
@@ -4,21 +4,22 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying Hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello Chris"
-
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
