9de82bde7fb3dac75704641f835757633d53dd00
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 14:30:06 +0400

    Add ExampleRepeat function to docs

--- learn-go-with-tests/iteration/repeat_test.go
+++ learn-go-with-tests/iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 5)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 10)
 	}
+}
+
+func ExampleRepeat() {
+	reps := Repeat("k", 10)
+	fmt.Println(reps)
+	//Output: kkkkkkkkkk
 }
