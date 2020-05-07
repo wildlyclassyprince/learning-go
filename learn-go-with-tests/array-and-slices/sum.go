0a3c264d44c4ed3e07e26bee6263710f2d2b8127
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 16:06:30 +0400

    Define SumAll function

--- learn-go-with-tests/array-and-slices/sum.go
+++ learn-go-with-tests/array-and-slices/sum.go
@@ -3,8 +3,25 @@ package main
 // Sum adds elements of an array and returns the result
 func Sum(numbers []int) int {
 	sum := 0
+
 	for _, number := range numbers {
 		sum += number
 	}
+
 	return sum
+}
+
+/*
+SumAll takes a varying number of slices and returns
+a new slice containing the totals for each slice passed in.
+*/
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
--- learn-go-with-tests/array-and-slices/sum_test.go
+++ learn-go-with-tests/array-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -13,4 +16,12 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
