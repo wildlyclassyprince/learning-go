2e9d739fbaa2d1862f13452f4f5037e33168b48c
wildlyclassyprince <lihtumb@gmail.com>
2020-05-07 14:26:32 +0400

    Specify number of repetitions

--- learn-go-with-tests/iteration/repeat.go
+++ learn-go-with-tests/iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-// Repeat a string character 5 times
-func Repeat(character string) string {
+// Repeat a string character N times
+func Repeat(character string, number int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < number; i++ {
 		repeated += character
 	}
 	return repeated
