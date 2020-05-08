66cba7bbe9f4d7c99ecaa17a6016f59b9e5aecbb
wildlyclassyprince <lihtumb@gmail.com>
2020-05-09 01:58:27 +0400

    Add spy sleeper for tests, and real sleeper to main()

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -11,15 +11,29 @@ const finalWord = "Go!"
 const countdownStart = 3
 
 // Countdown - prints a value and waits one second before printing the next
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+// Sleeper adds a delay to on our countdown
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper struct
+type DefaultSleeper struct{}
+
+// Sleep method implements Sleeper interface
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
--- learn-go-with-tests/mocking/countdown_test.go
+++ learn-go-with-tests/mocking/countdown_test.go
@@ -7,8 +7,9 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -19,4 +20,17 @@ Go!`
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	}
+}
+
+// SpySleeper 'spies' on our tests - removes sleeper delay
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
 }
