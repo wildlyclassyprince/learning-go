7f1d3e1ca1a22fefde16caaf2425090ac57f7223
wildlyclassyprince <lihtumb@gmail.com>
2020-05-09 01:32:25 +0400

    Test and create Countdown function

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -3,6 +3,6 @@ package main
 import "bytes"
 
 // Countdown - prints a value and waits one second before printing the next
-func Countdown(buffer *bytes.Buffer) string {
-	return ""
+func Countdown(buffer *bytes.Buffer) {
+
 }
