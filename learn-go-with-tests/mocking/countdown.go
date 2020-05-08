25099363542f9d28f33459627026bb2072ecbcbe
wildlyclassyprince <lihtumb@gmail.com>
2020-05-09 01:43:43 +0400

    Add 'for' loop, 'time.Sleep' and create named constants

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -1,8 +1,25 @@
 package main
 
-import "bytes"
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
 
 // Countdown - prints a value and waits one second before printing the next
-func Countdown(buffer *bytes.Buffer) {
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
 
+func main() {
+	Countdown(os.Stdout)
 }
