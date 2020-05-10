75f9951209e79b998dbffcc4b77d39a63890e99b
wildlyclassyprince <lihtumb@gmail.com>
2020-05-10 14:24:46 +0400

    Add go channel to handle DATA RACE on write to map[string]bool

--- learn-go-with-tests/concurrency/concurrency.go
+++ learn-go-with-tests/concurrency/concurrency.go
@@ -1,21 +1,29 @@
 package concurrency
 
-import "time"
-
 // WebsiteChecker takes a single URL and returns a boolean
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 // CheckWebsites checks the status of a list of URLs
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(websiteURL string) {
-			results[websiteURL] = wc(websiteURL)
+			// Send statement
+			resultChannel <- result{websiteURL, wc(websiteURL)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
