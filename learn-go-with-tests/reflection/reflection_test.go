c22a152fa705d8d265ae3aeadc54dd3c7bbf9bef
wildlyclassyprince <lihtumb@gmail.com>
2020-05-10 18:22:50 +0400

    Test for out-of-order maps

--- learn-go-with-tests/reflection/reflection_test.go
+++ learn-go-with-tests/reflection/reflection_test.go
@@ -88,6 +88,35 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+
+		t.Run("with maps", func(t *testing.T) {
+			aMap := map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			}
+
+			var got []string
+			walk(aMap, func(input string) {
+				got = append(got, input)
+			})
+
+			assertContains(t, got, "Bar")
+			assertContains(t, got, "Boz")
+		})
+	}
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
 
