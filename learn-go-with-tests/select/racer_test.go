61aef3fad88d2d20d4e9b3ad340d9f6af4de1eac
wildlyclassyprince <lihtumb@gmail.com>
2020-05-10 15:28:35 +0400

    Initial commit - test and create Racer function

---
+++ learn-go-with-tests/select/racer_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer
+	fastURL := fastServer
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
