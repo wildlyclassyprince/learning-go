fd65ffaf8a89bd1f90586cc6f8dbf42cbde4eddb
wildlyclassyprince <lihtumb@gmail.com>
2020-05-10 13:45:41 +0400

    Create slow test for website availability

--- learn-go-with-tests/concurrency/concurrency_test.go
+++ learn-go-with-tests/concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -16,7 +17,7 @@ func TestCheckWebsites(t *testing.T) {
 	websites := []string{
 		"http://google.com",
 		"http://blog.gypsydave5.com",
-		"waat://hurhurterwe.geds",
+		"waat://furhurterwe.geds",
 	}
 
 	want := map[string]bool{
@@ -29,5 +30,21 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("Wanted %v, got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
