be78b01b15723f2809029eac6deb3c8cfbe1adcb
wildlyclassyprince <lihtumb@gmail.com>
2020-05-11 20:39:15 +0400

    Add support for converting 4 to IV

--- learn-go-with-tests/intro-to-property-based-tests/numeral.go
+++ learn-go-with-tests/intro-to-property-based-tests/numeral.go
@@ -4,6 +4,9 @@ import "strings"
 
 // ConvertToRoman converts Arabic numerals to Roman numerals
 func ConvertToRoman(arabic int) string {
+	if arabic == 4 {
+		return "IV"
+	}
 	var result strings.Builder
 
 	for i := 0; i < arabic; i++ {
