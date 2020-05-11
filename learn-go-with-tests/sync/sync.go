2e1926a66a03923c8e1c3391a03df9e97420103d
wildlyclassyprince <lihtumb@gmail.com>
2020-05-11 19:45:39 +0400

    Add and test support for safe use with concurrency with 'sync' package

--- learn-go-with-tests/sync/sync.go
+++ learn-go-with-tests/sync/sync.go
@@ -1,12 +1,17 @@
 package sync
 
+import "sync"
+
 // Counter counts
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 // Inc increments value by 1
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
