981556d0b5a2d1c051c99e2f525df108f8105719
wildlyclassyprince <lihtumb@gmail.com>
2020-05-11 20:34:14 +0400

    Add support for converting 3 to III

--- learn-go-with-tests/intro-to-property-based-tests/numeral.go
+++ learn-go-with-tests/intro-to-property-based-tests/numeral.go
@@ -2,6 +2,9 @@ package main
 
 // ConvertToRoman converts Arabic numerals to Roman numerals
 func ConvertToRoman(arabic int) string {
+	if arabic == 3 {
+		return "III"
+	}
 	if arabic == 2 {
 		return "II"
 	}
