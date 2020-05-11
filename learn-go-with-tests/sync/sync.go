b0dc2f16cc8834e224893e162d622773fdeb2978
wildlyclassyprince <lihtumb@gmail.com>
2020-05-11 19:35:20 +0400

    Add struct value and type, Inc() function, and Value() returned

--- learn-go-with-tests/sync/sync.go
+++ learn-go-with-tests/sync/sync.go
@@ -1,13 +1,16 @@
 package sync
 
 // Counter counts
-type Counter struct{}
+type Counter struct {
+	value int
+}
 
 // Inc increments value by 1
 func (c *Counter) Inc() {
+	c.value++
 }
 
 // Value returns final value
 func (c *Counter) Value() int {
-	return 0
+	return c.value
 }
