1fb02568f8149a977eb5483423ff6f987e835cb8
wildlyclassyprince <lihtumb@gmail.com>
2020-05-11 20:10:57 +0400

    Initial commit - TestHandler

---
+++ learn-go-with-tests/context/context.go
@@ -0,0 +1,18 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Server(store Store) http.HandleFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		store.Cancel()
+		fmt.Fprint(w, store.Fetch())
+	}
+}
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
