e8c2f21aed4e6ff01c789705428686da8c76c0c4
wildlyclassyprince <lihtumb@gmail.com>
2020-08-05 11:55:59 +0400

    Initial commit - intro to templates part 2

---
+++ intro-to-templates-in-go/template-actions-and-nested-templates/README.md
@@ -0,0 +1,5 @@
+# Template Actions and Nested Templates in Go
+
+Part 2 of the Intro to Templates in Go series.
+
+Up until now we have been outputting templates to the terminal, but as we start to dive into more HTML this starts to make less sense. Instead we want to visualize the HTML being generated in a web browser. To do this we first need to set up a web server that will render our HTML templates.
---
+++ intro-to-templates-in-go/template-actions-and-nested-templates/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var testTemplate *template.Template
+
+// ViewData is simple testing struct
+type ViewData struct {
+	Name string
+}
+
+func main() {
+	var err error
+	testTemplate, err = template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	vd := ViewData{"John Smith"}
+	err := testTemplate.Execute(w, vd)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
