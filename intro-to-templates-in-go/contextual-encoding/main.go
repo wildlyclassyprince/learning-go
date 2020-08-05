9822d50b2090520440fdd94138b087c6c0f6ace9
wildlyclassyprince <lihtumb@gmail.com>
2020-08-05 11:19:50 +0400

    Hello, John Smith

---
+++ intro-to-templates-in-go/contextual-encoding/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	data := struct {
+		Name string
+	}{"John Smith"}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+}
