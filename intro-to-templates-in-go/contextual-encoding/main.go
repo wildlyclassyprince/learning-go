76ccceb9fcf1da099cf66db8fd39ee7df611c34a
wildlyclassyprince <lihtumb@gmail.com>
2020-08-05 11:43:20 +0400

    Test encoding

--- intro-to-templates-in-go/contextual-encoding/main.go
+++ intro-to-templates-in-go/contextual-encoding/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"html/template"
 	"os"
-	"text/template"
 )
 
+// Test simple struct for testing
+type Test struct {
+	HTML     string
+	SafeHTML template.HTML
+	Title    string
+	Path     string
+	Dog      Dog
+	Map      map[string]string
+}
+
+// Dog simple struct for testing
+type Dog struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	t, err := template.ParseFiles("context.gohtml")
 	if err != nil {
 		panic(err)
 	}
 
-	data := struct {
-		Name string
-	}{"John Smith"}
+	data := Test{
+		HTML:     "<h1>A header!</h1>",
+		SafeHTML: template.HTML("<h1>A Safe header</h1>"),
+		Title:    "Backslash! An in depth look at the \"\\\" character.",
+		Path:     "/dashboard/settings",
+		Dog:      Dog{"Fido", 6},
+		Map: map[string]string{
+			"key":       "value",
+			"other_key": "other_value",
+		},
+	}
 
 	err = t.Execute(os.Stdout, data)
 	if err != nil {
